c20e1a7025f84d5325a18711963eabc15f3892ba
minhfix <taikhoanhoctap.10@gmail.com>
2021-03-18 11:54:17 +0900

    router - controller

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ const path = require('path');
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars');
+const { log } = require('console');
 
 
 const app = express();
 const port = 3000;
 
+const route  = require('./routes');
+
+
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // template engine 
 app.engine('hbs', handlebars({
@@ -21,13 +29,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
