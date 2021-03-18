242ced817220da60fca7b13ef2a6dd3aa04acbc1
minhfix <taikhoanhoctap.10@gmail.com>
2021-03-18 17:14:55 +0900

    pettier husky lint-stages

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,12 +1,11 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
 const newsController = require('../app/controllers/NewsController');
 
 // newController.index
 
-router.use('/:slug', newsController.show)
-router.use('/', newsController.index)
+router.use('/:slug', newsController.show);
+router.use('/', newsController.index);
 
-module.exports = router
-
+module.exports = router;
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,12 +1,11 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-const siteController= require('../app/controllers/SiteController');
+const siteController = require('../app/controllers/SiteController');
 
 // newController.index
 
-router.use('/search', siteController.search)
-router.use('/', siteController.index)
+router.use('/search', siteController.search);
+router.use('/', siteController.index);
 
-module.exports = router
-
+module.exports = router;
